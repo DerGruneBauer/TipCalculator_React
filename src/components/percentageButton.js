1fd9c27219f9f6434edba080758323d05454647f
remington <remigreenbauer@gmail.com>
2021-08-17 22:24:13 -0400

    additional styling added along with input custom button

--- src/components/percentageButton.js
+++ src/components/percentageButton.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "../styles/percentageButton.module.css";
 
 const PercentageButton = (props) => (
-  <div onClick={props.onClick} value={props.value} className={styles.percentageButton}>{props.textValue}</div>
+  <div color={props.color} onClick={props.onClick} value={props.value} className={styles.percentageButton}>{props.textValue}</div>
 );
 
 export default PercentageButton;
